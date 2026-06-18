bfdef8ab66b11a795ff21ce922eb7b4b5a517a40
Muhammad Arifan L <arifmuhammad.idn@gmail.com>
2026-06-18 20:29:28 +0700

    feat: add default value for subdomain column in events table migration

--- database/migrations/2026_06_16_123117_add_subdomain_to_events_table.php
+++ database/migrations/2026_06_16_123117_add_subdomain_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('subdomain')->unique()->after('name');
+            $table->string('subdomain')->unique()->after('name')->default('');
         });
     }
 
