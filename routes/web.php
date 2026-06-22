6f925902097eacfa20dddaa0cb6aa5ebf6607f90
Muhammad Arifan L <arifmuhammad.idn@gmail.com>
2026-06-23 02:57:07 +0700

    fix: replace hardcoded domain suffix with configurable app URL

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ if (config('app.env') === 'local') {
     Route::get('/{event}/check-contribution', [EventController::class, 'checkContribution'])->name('events.check-contribution');
     Route::get('/{event}', [EventController::class, 'show'])->name('events.show');
 } else {
-    Route::domain('{event}.bl.test')->group(function () {
+    Route::domain('{event}.' . config('app.url'))->group(function () {
         Route::get('/', [EventController::class, 'show'])->name('events.show');
         Route::get('/transactions', [EventController::class, 'transactions'])->name('events.transactions');
         Route::get('/check-contribution', [EventController::class, 'checkContribution'])->name('events.check-contribution');
