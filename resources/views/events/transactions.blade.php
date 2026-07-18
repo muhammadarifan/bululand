e9c910ad35ed467ad86d3d10896f75cce5a7d1b1
Muhammad Arifan L <arifmuhammad.idn@gmail.com>
2026-07-18 21:18:32 +0700

    refactor: remove total contribution display for improved clarity

--- resources/views/events/transactions.blade.php
+++ resources/views/events/transactions.blade.php
@@ -114,12 +114,6 @@ $balance = $totalIncome - $totalExpense;
                                         d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
                             </div>
-                            <div class="min-w-0">
-                                <p class="text-[10px] font-semibold uppercase tracking-widest text-neutral-400">Total
-                                    Iuran Terkumpul</p>
-                                <p class="text-xl font-bold truncate">Rp {{ number_format($totalContribution, 0, ',',
-                                    '.') }}</p>
-                            </div>
                         </div>
                     </div>
                     @endif
